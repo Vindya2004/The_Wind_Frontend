cdbede127c4b06a5b6c49467d204af879202e41e
Vindya2004 <vindyamadubhashini@gmail.com>
2025-12-09 15:01:48 +0530

    Ignore node_modules and .env

---
+++ src/components/Common/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <header>
+        {/*topbar */}
+        <Topbar/>
+        {/*navbar */}
+        <Navbar/>
+        {/*cart drawer */}
+    </header>
+  )
+}
+
+export default Header

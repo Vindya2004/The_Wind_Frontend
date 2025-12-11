83945c4c91c35d50ec7e9e344651c8f1130d8008
Vindya2004 <vindyamadubhashini@gmail.com>
2025-12-12 00:05:25 +0530

    add login

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserLayout from './components/Layout/UserLayout';
 import Home from './pages/Home';
 import { Toaster } from 'sonner';
+import Login from './pages/Login';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
    <Routes>
     <Route path='/' element={<UserLayout/>}>
     <Route index element={<Home/>}/>
+    <Route path='login' element={<Login/>}/>
     {/*User Layout */}
     </Route>
     <Route>{/*Admin Layout */}</Route>

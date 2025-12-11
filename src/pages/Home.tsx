3eabe08675bc3b9b392c09215300740881b71256
Vindya2004 <vindyamadubhashini@gmail.com>
2025-12-11 22:09:54 +0530

    add product_details section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,41 @@ import React from 'react'
 import Hero from '../components/Layout/Hero'
 import GenderCollectionSection from '../components/Products/GenderCollectionSection'
 import NewArrivals from '../components/Products/NewArrivals'
+import ProductDetails from '../components/Products/ProductDetails'
+import ProductGrid from '../components/Products/ProductGrid'
+
+const placeholderProducts = [
+   {
+        _id: 4,
+        name: "Product 1",
+        price: 100,
+        images:[{url:"https://picsum.photos/500/500?random=3"}],
+    },
+     {
+        _id: 5,
+        name: "Product 1",
+        price: 100,
+        images:[{url:"https://picsum.photos/500/500?random=4"}],
+    },
+     {
+        _id: 6,
+        name: "Product 1",
+        price: 100,
+        images:[{url:"https://picsum.photos/500/500?random=5"}],
+    },
+     {
+        _id: 7,
+        name: "Product 1",
+        price: 100,
+        images:[{url:"https://picsum.photos/500/500?random=6"}],
+    },
+      {
+        _id: 8,
+        name: "Product 1",
+        price: 100,
+        images:[{url:"https://picsum.photos/500/500?random=6"}],
+    },
+]
 
 const Home = () => {
   return (
@@ -9,6 +44,19 @@ const Home = () => {
         <Hero/>
         <GenderCollectionSection />
         <NewArrivals />
+
+        {/* Best Seller */}
+        <h2 className='text-3xl text-center font-bold mb-4'>
+          Best Seller
+        </h2>
+        <ProductDetails />
+
+        <div className='container mx-auto'>
+          <h2 className='text-3xl text-center font-bold mb-4'>
+            Sandals For Women 
+          </h2>
+          <ProductGrid products={placeholderProducts} />
+        </div>
     </div>
   )
 }

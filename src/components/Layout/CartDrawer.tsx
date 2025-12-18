6473b0fc20d95941309c933fd2510995ccf7aaf9
Vindya2004 <vindyamadubhashini@gmail.com>
2025-12-18 23:43:10 +0530

    add collection

--- src/components/Layout/CartDrawer.tsx
+++ src/components/Layout/CartDrawer.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import { IoMdClose } from 'react-icons/io';
 import CartContent from '../Cart/CartContent';
+import { useNavigate } from 'react-router-dom';
 
 const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
+    const navigate = useNavigate();
+    const handleCheckout = () => {
+        navigate("/checkout")
+    }
    
 
   return (
@@ -26,7 +31,7 @@ const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
             </div>
             {/* Checkout button fixed at the bottom */}
             <div className='p-4 bg-white sticky bottom-0'>
-                <button className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800
+                <button onClick={handleCheckout} className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800
                 transition'>
                     Checkout
                 </button>
